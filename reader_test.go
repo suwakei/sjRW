b67408012741ecaabd92e78bc38f7e1b5e82935b
keitosuwahara <210103gi@yse-c.net>
2024-11-16 22:38:38 +0900

    TestDiff moved to wroter_test

--- reader_test.go
+++ reader_test.go
@@ -1,11 +1,8 @@
 package sjrw
 
 import (
-	"fmt"
 	"log"
 	"testing"
-
-	"github.com/suwakei/sjrw/internal"
 )
 
 
@@ -30,15 +27,4 @@ func BenchmarkReadAsBytes(b *testing.B) {
 		log.Fatal(err)
 	}
 }
-}
-
-func TestDiff(t *testing.T) {
-	var jsonPath1 string = "./testdata/difftest.json"
-	var jsonPath2 string = "./testdata/difftest2.json"
-	sj1 := Sj{ReadJsonPath: jsonPath1}
-	sj2 := Sj{ReadJsonPath: jsonPath2}
-	s1, _ := sj1.ReadAsBytes()
-	s2, _ := sj2.ReadAsBytes()
-	i := internal.Diff("test1", s1, "test2", s2)
-	fmt.Println(string(i))
 }
