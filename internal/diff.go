16db63f63bec906062644d575571cfc4105f5dd2
keitosuwahara <210103gi@yse-c.net>
2024-11-17 12:59:16 +0900

    test passed

--- internal/diff.go
+++ internal/diff.go
@@ -11,28 +11,21 @@ import (
 type pair struct{ x, y int }
 
 
-// Diff returns 
-func Diff[bytesOrStr []bytes | string] (oldName string, old bytesOrStr, newName string, new bytesOrStr) []byte {
-	if bytes.Equal(old, new) {
-		fmt.Println("old and new are the same value")
-		return nil
+// Diff returns result of comparing "from" and "to" "rm" line list and "add" line list as two dimention map
+// and diff log as []byte
+func Diff (fromName string, from []byte, toName string, to []byte) (map[string]map[int]string, []byte) {
+	if bytes.Equal(from, to) {
+		fmt.Printf("%s and %s are the same value", fromName, toName)
+		return nil, nil
 	}
 
-	x := lines(old)
-	y := lines(new)
+	x := lines(from)
+	y := lines(to)
 
 	// Print diff header.
 	var out bytes.Buffer
-	fmt.Fprintf(&out, "diff %s %s\n", oldName, newName)
-	fmt.Fprintf(&out, "--- %s\n", oldName)
-	fmt.Fprintf(&out, "+++ %s\n", newName)
+	fmt.Fprintf(&out, "diff '%s' '%s'\n", fromName, toName)
 
-	// Loop over matches to consider,
-	// expanding each match to include surrounding lines,
-	// and then printing diff chunks.
-	// To avoid setup/teardown cases outside the loop,
-	// tgs returns a leading {0,0} and trailing {len(x), len(y)} pair
-	// in the sequence of matches.
 	var (
 		done  pair     // printed up to x[:done.x] and y[:done.y]
 		chunk pair     // start lines of current chunk
@@ -40,16 +33,18 @@ func Diff[bytesOrStr []bytes | string] (oldName string, old bytesOrStr, newName 
 		ctext []string // lines for current chunk
 	)
 
+	const spl string = "------------------------------------------------------\n"
+
 	for _, m := range tgs(x, y) {
 		if m.x < done.x {
 			// Already handled scanning forward from earlier match.
 			continue
 		}
 
-		// Expand matching lines as far as possible,
-		// establishing that x[start.x:end.x] == y[start.y:end.y].
-		// Note that on the first (or last) iteration we may (or definitely do)
-		// have an empty match: start.x==end.x and start.y==end.y.
+			// Expand matching lines as far as possible,
+			// establishing that x[start.x:end.x] == y[start.y:end.y].
+			// Note that on the first (or last) iteration we may (or definitely do)
+			// have an empty match: start.x==end.x and start.y==end.y.
 		start := m
 		for start.x > done.x && start.y > done.y && x[start.x-1] == y[start.y-1] {
 			start.x--
@@ -66,12 +61,12 @@ func Diff[bytesOrStr []bytes | string] (oldName string, old bytesOrStr, newName 
 		// Emit the mismatched lines before start into this chunk.
 		// (No effect on first sentinel iteration, when start = {0,0}.)
 		for _, s := range x[done.x:start.x] {
-			ctext = append(ctext, "-"+s)
+			ctext = append(ctext, "-" + s)
 			count.x++
 		}
 
 		for _, s := range y[done.y:start.y] {
-			ctext = append(ctext, "+"+s)
+			ctext = append(ctext, "+" + s)
 			count.y++
 		}
 
@@ -80,10 +75,10 @@ func Diff[bytesOrStr []bytes | string] (oldName string, old bytesOrStr, newName 
 		const C = 3 // number of context lines
 
 		if (end.x < len(x) || end.y < len(y)) &&
-			(end.x-start.x < C || (len(ctext) > 0 && end.x-start.x < 2*C)) {
+			(end.x - start.x < C || (len(ctext) > 0 && end.x - start.x < 2*C)) {
 
 			for _, s := range x[start.x:end.x] {
-				ctext = append(ctext, " "+s)
+				ctext = append(ctext, " " + s)
 				count.x++
 				count.y++
 			}
@@ -100,8 +95,8 @@ func Diff[bytesOrStr []bytes | string] (oldName string, old bytesOrStr, newName 
 				n = C
 			}
 
-			for _, s := range x[start.x : start.x+n] {
-				ctext = append(ctext, " "+s)
+			for _, s := range x[start.x : start.x + n] {
+				ctext = append(ctext, " " + s)
 				count.x++
 				count.y++
 			}
@@ -119,7 +114,10 @@ func Diff[bytesOrStr []bytes | string] (oldName string, old bytesOrStr, newName 
 				chunk.y++
 			}
 
-			fmt.Fprintf(&out, "@@ -%d,%d +%d,%d @@\n", chunk.x + 3, count.x, chunk.y + 3, count.y)
+			fmt.Fprintf(&out, "@@ '%s' %d,%d | '%s' %d,%d @@\n", fromName, chunk.x + 2, count.x, toName, chunk.y + 3, count.y)
+
+			out.WriteString(fmt.Sprintf("Lines being removed or added from [%s]\n", fromName))
+			out.WriteString(spl)
 
 			for _, s := range ctext {
 				out.WriteString(s)
@@ -145,10 +143,40 @@ func Diff[bytesOrStr []bytes | string] (oldName string, old bytesOrStr, newName 
 		}
 
 		done = end
-		fmt.Println(done)
 	}
 
-	return out.Bytes()
+	var editMap map[string]map[int]string = make(map[string]map[int]string)
+	// prepare two dimenton map for return value
+	if _, ok := editMap["rm"]; !ok {
+		editMap["rm"] = make(map[int]string)
+	}
+
+	if _, ok := editMap["add"]; !ok {
+		editMap["add"] = make(map[int]string)
+	}
+
+	str := out.String()
+	trim := strings.TrimSpace(str)
+	ar := strings.Split(trim, spl)[1:]
+
+	var strarr []string
+	for i := range len(ar) {
+		strarr = strings.Split(ar[i], "\n")
+	}
+
+	for idx, s := range strarr {
+		flag := strings.Split(s, "")[0:1][0]
+		if flag ==  "-" {
+			editMap["rm"][idx + 1] = s
+		}
+
+		if flag == "+" {
+			editMap["add"][idx + 1] = s
+		}
+	}
+
+	var log []byte = out.Bytes()
+	return editMap, log
 }
 
 // lines returns the lines in the file x, including newlines.
@@ -160,21 +188,11 @@ func lines(x []byte) []string {
 	if l[len(l) - 1] == "" {
 		l = l[:len(l) - 1]
 	} else {
-		// Treat last line as having a message about the missing newline attached,
-
 		l[len(l)-1] += "\n\\ No newline at end of file\n"
 	}
 	return l
 }
 
-// tgs returns the pairs of indexes of the longest common subsequence
-// of unique lines in x and y, where a unique line is one that appears
-// once in x and once in y.
-//
-// The longest common subsequence algorithm is as described in
-// Thomas G. Szymanski, “A Special Case of the Maximal Common
-// Subsequence Problem,” Princeton TR #170 (January 1975),
-// available at https://research.swtch.com/tgs170.pdf.
 func tgs(x, y []string) []pair {
 	// Count the number of times each string appears in a and b.
 	// We only care about 0, 1, many, counted as 0, -1, -2
