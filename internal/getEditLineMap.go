3f4a4c6b7859639323d08f20cc5fec3c1c95716e
keitosuwahara <210103gi@yse-c.net>
2024-11-19 06:17:45 +0900

    moved func getEditLine from sjrw to internal

---
+++ internal/getEditLineMap.go
@@ -0,0 +1,48 @@
+package internal
+
+import (
+	"strings"
+	"sort"
+)
+
+
+
+// getEditLineMap returns a map with only the difference changed from the original string
+func GetEditLineMap[targetType []byte | string] (str targetType, editMapFromDiff map[string]map[int]string) map[int]string {
+	content := strings.TrimSpace(string(str))
+	contentLines := strings.Split(content, "\n")
+
+	indexes := make(map[int]string)
+	for i, line := range contentLines {
+		indexes[i + 1] = line
+	}
+
+	rm := editMapFromDiff["rm"]
+	RMNUMS := GetKey(rm)
+	sort.Ints(RMNUMS)
+
+	add := editMapFromDiff["add"]
+	ADDNUMS := GetKey(add)
+	sort.Ints(ADDNUMS)
+
+	// remove
+	for _, rmn := range RMNUMS {
+		delete(indexes, rmn)
+	}
+
+	// add
+	for _, addn := range ADDNUMS {
+		indexes[addn] = editMapFromDiff["add"][addn]
+	}
+
+	return indexes
+}
+
+
+func GetKey(m map[int]string) []int {
+	var arr []int
+	for k, _ := range m {
+		arr = append(arr, k)
+	}
+	return arr
+}
