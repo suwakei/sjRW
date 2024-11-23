df7a8ef306cdd7eb0028d82d6931291c8a97c072
keitosuwahara <210103gi@yse-c.net>
2024-11-23 15:55:10 +0900

    .

--- reader.go
+++ reader.go
@@ -13,7 +13,7 @@ type SjReader struct {
 }
 
 //ReadAsStr returns json content as string
-func (sj *SjReader) ReadAsStrFrom(readFilePath string) (string, error) {
+func (sj *SjReader) ReadAsStrFrom(readFilePath string) (contentAsStr string, err error) {
 	var jsonByte []byte
 	path := readFilePath
 
@@ -44,11 +44,12 @@ func (sj *SjReader) ReadAsStrFrom(readFilePath string) (string, error) {
 		}
 
 	}
-	return strings.TrimSpace(string(jsonByte)), err
+	contentAsStr = strings.TrimSpace(string(jsonByte))
+	return contentAsStr, err
 }
 
 
-func (sj *SjReader) ReadAsBytesFrom(readFilePath string) ([]byte, error) {
+func (sj *SjReader) ReadAsByteFrom(readFilePath string) (contentAsByte []byte, err error) {
 	var jsonByte []byte
 	path := readFilePath
 
@@ -78,5 +79,42 @@ func (sj *SjReader) ReadAsBytesFrom(readFilePath string) ([]byte, error) {
 			panic(err)
 		}
 	}
-	return jsonByte, err
+	contentAsByte = jsonByte
+
+	return contentAsByte, err
+}
+
+
+func (sj *SjReader) ReadAsMapFrom(readFilePath string) (contentAsMap map[string]interface{}, err error) {
+	var jsonByte []byte
+	path := readFilePath
+
+	if _, err := os.Stat(path); err != nil {
+		log.Fatalf("this path is not exist %s", path)
+	}
+
+	f, err := os.OpenFile(readFilePath, os.O_RDONLY, 0666)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	reader := bufio.NewReaderSize(f, 24 * 1024)
+	for {
+		readByte, _, err := reader.ReadLine()
+
+		jsonByte = append(jsonByte, append(readByte, []byte("\n")...)...)
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// 行の順番を持ったマップとjsonのキーをキーとしたマップ
 }
