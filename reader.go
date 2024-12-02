474da9d11bf3924e109e3c94c2462b640105246e
keitosuwahara <210103gi@yse-c.net>
2024-12-03 06:46:26 +0900

    str to ioreader

--- reader.go
+++ reader.go
@@ -2,7 +2,6 @@ package sjrw
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -18,27 +17,6 @@ type SjReader struct {
 //ReadAsStr returns json content as string
 func (SjReader) ReadAsStrFrom(readFile io.Reader) (contentAsStr string, err error) {
 	var jsonByte []byte = make([]byte, 200)
-
-	// if _, err := os.Stat(readFilePath); err != nil {
-	// 	log.Fatalf("this path is not exist \"%s\"", readFilePath)
-	// }
-
-	// if filepath.Ext(readFilePath) != ".json" {
-	// 	return "", errors.New("read file is not json file")
-	// }
-
-	// f, err := os.OpenFile(readFilePath, os.O_RDONLY, 0o666)
-
-	// if err != nil {
-	// 	log.Fatalf("could not open file \"%s\"", readFilePath)
-	// }
-
-	// defer func() {
-	// 	err := f.Close()
-	// 	if err != nil {
-	// 		log.Fatalf("could not close file \"%s\"", readFilePath)
-	// 	}
-	// }()
 
 	reader := bufio.NewReaderSize(readFile, 24 * 1024)
 
@@ -63,27 +41,6 @@ func (SjReader) ReadAsStrFrom(readFile io.Reader) (contentAsStr string, err erro
 func (SjReader) ReadAsByteFrom(readFile io.Reader) (contentAsByte []byte, err error) {
 	var jsonByte []byte = make([]byte, 200)
 
-	// if _, err := os.Stat(readFilePath); err != nil {
-	// 	log.Fatalf("this path is not exist \"%s\"", readFilePath)
-	// }
-
-	// if filepath.Ext(readFilePath) != ".json" {
-	// 	return []byte(""), errors.New("read file is not json file")
-	// }
-
-	// f, err := os.OpenFile(readFilePath, os.O_RDONLY, 0o666)
-
-	// if err != nil {
-	// 	log.Fatalf("could not open file \"%s\"", readFilePath)
-	// }
-
-	// defer func() {
-	// 	err := f.Close()
-	// 	if err != nil {
-	// 		log.Fatalf("could not close file \"%s\"", readFilePath)
-	// 	}
-	// }()
-
 	reader := bufio.NewReaderSize(readFile, 24 * 1024)
 	for {
 		readByte, _, err := reader.ReadLine()
@@ -103,39 +60,19 @@ func (SjReader) ReadAsByteFrom(readFile io.Reader) (contentAsByte []byte, err er
 }
 
 
-// 本当に一行ずつ取得とかではなくて配列またはオブジェクトがキーの中にあったらそれをすべて参照できるようにする
 func (SjReader) ReadAsMapFrom(readFile io.Reader) (contentAsMap map[int]map[string]any, err error) {
 	var (
-		jsonByte []byte = make([]byte, 200)
+		jsonRune []rune = make([]rune, 0, 800)
 	)
 
-	// if _, err := os.Stat(readFilePath); err != nil {
-	// 	log.Fatalf("this path is not exist \"%s\"", readFilePath)
-	// }
-
-	// if filepath.Ext(readFilePath) != ".json" {
-	// 	return nil, errors.New("read file is not json file")
-	// }
-
-	// f, err := os.OpenFile(readFilePath, os.O_RDONLY, 0o666)
-
-	// if err != nil {
-	// 	log.Fatalf("could not open file \"%s\"", readFilePath)
-	// }
-
-	// defer func() {
-	// 	err := f.Close()
-	// 	if err != nil {
-	// 		log.Fatalf("could not close file \"%s\"", readFilePath)
-	// 	}
-	// }()
 
 	reader := bufio.NewReaderSize(readFile, 24 * 1024)
+	runeSlice := make([]rune, 0, 400)
 
 	for {
-		readByte, _, err := reader.ReadLine()
+		readRune, _, err := reader.ReadRune()
 
-		jsonByte = append(jsonByte, append(readByte, []byte("\n")...)...)
+		jsonRune = append(jsonRune, append(runeSlice, readRune)...)
 
 		if err == io.EOF {
 			break
@@ -145,12 +82,9 @@ func (SjReader) ReadAsMapFrom(readFile io.Reader) (contentAsMap map[int]map[stri
 			log.Fatal("could not read content")
 		}
 	}
-	trimedByte := bytes.TrimSpace(jsonByte)
-	st := string(trimedByte)
-	fmt.Println(st)
-	rs := []rune(st)
+	fmt.Println(len(jsonRune))
 
-	contentAsMap = internal.AssembleMap(rs)
+	contentAsMap = internal.AssembleMap(jsonRune)
 
 	return contentAsMap, err
 }
