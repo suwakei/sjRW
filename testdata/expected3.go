8a2b06c35da20f9d728c282172ae8e33a0e3740e
keitosuwahara <keitosuwahara0816@gmail.com>
2024-12-26 11:43:27 +0900

    added expected.go

---
+++ testdata/expected3.go
@@ -0,0 +1,9 @@
+package testdata
+
+const Expected3 string = 
+`{
+    "add": "git add .",
+    "commit": "git commit -m \"first commit\"",
+    "push": "git push origin main",
+    "status": "git status"
+}`
